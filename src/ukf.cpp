0705d10cf1b926a757ab3b1b0f91cf5aa2bcb47b
Ibis Prevedello <ibiscp@gmail.com>
2017-05-10 23:24:48 -0300

    feat: Add PredictSigmaPoints and PredictMeanAndCovariance

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -66,16 +66,22 @@ UKF::UKF() {
     int n_aug_ = 7;
 
     //define spreading parameter
-    double lambda_ = 3 - n_aug_;
-
-    // predicted sigma points matrix
-    Xsig_pred_ = MatrixXd(n_aug_, 2 * n_aug_ + 1);
-
-    //create sigma point matrix
-    MatrixXd Xsig = MatrixXd(n_x_, 2 * n_x_ + 1);
+    lambda_ = 3 - n_aug_;
 
     //create sigma point matrix
     MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+
+    //create matrix with predicted sigma points as columns
+    MatrixXd Xsig_pred = MatrixXd(n_x_, 2 * n_aug_ + 1);
+
+    //create vector for weights
+    VectorXd weights = VectorXd(2*n_aug_+1);
+
+    //create vector for predicted state
+    VectorXd x = VectorXd(n_x_);
+
+    //create covariance matrix for prediction
+    MatrixXd P = MatrixXd(n_x_, n_x_);
 
 }
 
@@ -151,6 +157,95 @@ void UKF::AugmentSigmaPoints(MatrixXd &Xsig_aug) {
         Xsig_aug.col(i+1)     = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
         Xsig_aug.col(i+1+n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
     }
+    return;
+}
+
+/**
+ * @param {MeasurementPackage} meas_package
+ * Calculates Predict Sigma Points
+ */
+void UKF::PredictSigmaPoints(MatrixXd &Xsig_aug, double delta_t, MatrixXd &Xsig_pred) {
+
+    //create matrix with predicted sigma points as columns
+    //MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+
+    //predict sigma points
+    for (int i = 0; i< 2*n_aug_+1; i++) {
+        //extract values for better readability
+        double p_x = Xsig_aug(0,i);
+        double p_y = Xsig_aug(1,i);
+        double v = Xsig_aug(2,i);
+        double yaw = Xsig_aug(3,i);
+        double yawd = Xsig_aug(4,i);
+        double nu_a = Xsig_aug(5,i);
+        double nu_yawdd = Xsig_aug(6,i);
+
+        //predicted state values
+        double px_p, py_p;
+
+        //avoid division by zero
+        if (fabs(yawd) > 0.001) {
+            px_p = p_x + v/yawd * ( sin (yaw + yawd*delta_t) - sin(yaw));
+            py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw+yawd*delta_t) );
+        } else {
+            px_p = p_x + v*delta_t*cos(yaw);
+            py_p = p_y + v*delta_t*sin(yaw);
+        }
+
+        double v_p = v;
+        double yaw_p = yaw + yawd*delta_t;
+        double yawd_p = yawd;
+
+        //add noise
+        px_p = px_p + 0.5*nu_a*delta_t*delta_t * cos(yaw);
+        py_p = py_p + 0.5*nu_a*delta_t*delta_t * sin(yaw);
+        v_p = v_p + nu_a*delta_t;
+
+        yaw_p = yaw_p + 0.5*nu_yawdd*delta_t*delta_t;
+        yawd_p = yawd_p + nu_yawdd*delta_t;
+
+        //write predicted sigma point into right column
+        Xsig_pred(0,i) = px_p;
+        Xsig_pred(1,i) = py_p;
+        Xsig_pred(2,i) = v_p;
+        Xsig_pred(3,i) = yaw_p;
+        Xsig_pred(4,i) = yawd_p;
+    }
+    return;
+}
+
+/**
+ * @param {MeasurementPackage} meas_package
+ * Predict Mean and Covariance
+ */
+void UKF::PredictMeanAndCovariance(MatrixXd &Xsig_pred, VectorXd &weights) {
+    // set weights
+    double weight_0 = lambda_/(lambda_+n_aug_);
+    weights(0) = weight_0;
+    for (int i=1; i<2*n_aug_+1; i++) {  //2n+1 weights
+        double weight = 0.5/(n_aug_+lambda_);
+        weights(i) = weight;
+    }
+
+    //predicted state mean
+    x.fill(0.0);
+    for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+        x = x+ weights(i) * Xsig_pred.col(i);
+    }
+
+    //predicted state covariance matrix
+    P.fill(0.0);
+    for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+
+        // state difference
+        VectorXd x_diff = Xsig_pred.col(i) - x;
+        //angle normalization
+        while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
+        while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
+
+        P = P + weights(i) * x_diff * x_diff.transpose() ;
+    }
+
     return;
 }
 
@@ -233,9 +328,13 @@ void UKF::Prediction(double delta_t) {
     */
 
     /// Augment sigma points
-    AugmentSigmaPoints(Xsig_aug)
+    AugmentSigmaPoints(Xsig_aug);
 
     /// Predict sigma points
+    PredictSigmaPoints(Xsig_aug, delta_t, Xsig_pred);
+
+    /// Predict mean and covariance
+    PredictMeanAndCovariance(Xsig_pred, weights);
 }
 
 /**
